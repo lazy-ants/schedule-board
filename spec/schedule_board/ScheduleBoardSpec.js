53979b4ba69274ed6348203f4004cdbf002721eb
Coffee-Tea <bazyukvitaliy@gmail.com>
2016-03-10 19:01:59 +0200

    Added tests for editing records

--- spec/schedule_board/ScheduleBoardSpec.js
+++ spec/schedule_board/ScheduleBoardSpec.js
@@ -111,4 +111,182 @@ describe('addRecordDrtc', function() {
             expect(scope.newRecord.time).toEqual("");
         });
     });
+
+    describe('Adding of records is working', function() {
+
+        beforeEach(function() {
+            scope.checkTitleValidation = function () { return true; };
+            scope.showInformModal = function () { return };
+            scope.chosenDate = "10 March 2016";
+            scope.days = {
+                "10 March 2016": [
+                    {
+                        time: "08:00 - 09:00",
+                        title: "-",
+                    },
+                    {
+                        time: "09:00 - 10:00",
+                        title: "Phil Rasseliny"
+                    }
+                ]
+            };
+        });
+
+        describe("scope.addRecord() calls function applyNewTitle ()", function () {
+
+            it("function applyNewTitle () adds record to correct time", function () {
+                scope.newRecord = {
+                    title: "Sergey Ivanov",
+                    time: "08:00 - 09:00"
+                };
+                scope.addRecord();
+                expect(scope.days[scope.chosenDate][0]).toBeDefined();
+                expect(scope.days[scope.chosenDate][0].time).toEqual("08:00 - 09:00");
+                expect(scope.days[scope.chosenDate][0].title).toEqual("Sergey Ivanov");
+            });
+
+            it("function applyNewTitle () does not add record to incorrect time", function () {
+                scope.newRecord = {
+                    title: "Sergey Ivanov",
+                    time: "09:00 - 10:00"
+                };
+                scope.addRecord();
+                expect(scope.days[scope.chosenDate][0]).toBeDefined();
+                expect(scope.days[scope.chosenDate][0].time).toEqual("08:00 - 09:00");
+                expect(scope.days[scope.chosenDate][0].title).toEqual("-");
+            });
+
+            describe("function applyNewTitle () calls function resetNewRecord ()", function () {
+                it("function applyNewTitle () adds record to correct time", function () {
+                    scope.newRecord = {
+                        title: "Sergey Ivanov",
+                        time: "08:00 - 09:00"
+                    };
+                    scope.addRecord();
+                    expect(scope.newRecord).toBeDefined();
+                    expect(scope.newRecord.title).toEqual("");
+                    expect(scope.newRecord.time).toEqual("");
+                });
+
+                it("function applyNewTitle () does not add incorrect record into schedule", function () {
+                    scope.newRecord = {
+                        title: "Sergey Ivanov",
+                        time: "09:00 - 10:00"
+                    };
+                    scope.addRecord();
+                    expect(scope.newRecord).toBeDefined();
+                    expect(scope.newRecord.title).toEqual("Sergey Ivanov");
+                    expect(scope.newRecord.time).toEqual("09:00 - 10:00");
+                });
+            });
+        });
+    });
+});
+
+describe('editRecordDrtc', function() {
+    var $compile,
+            $rootScope,
+            scope,
+            element;
+    beforeEach(module('app'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_, $templateCache){
+        $templateCache.put('testView.html', [
+              '<span id="edit" class="glyphicon glyphicon-pencil" role="button" ng-click="showEditForm(record)"></span>'
+            ].join(''));
+        $rootScope = _$rootScope_;
+        scope = $rootScope.$new();
+        $compile = _$compile_;
+        element = $compile("<edit-record></edit-record>")(scope);
+        scope.$digest();
+
+    }));
+
+    it('Object scope.editingRecord is null', function() {
+        expect(scope.editingRecord).toBeNull();
+    });
+
+    it('Clicks on "edit" open Edit Form', function() {
+        spyOn(scope, 'showEditForm');
+        simpleHtml = '<span id="edit" class="glyphicon glyphicon-pencil" role="button" ng-click="showEditForm(record)"></span>';
+        el = $compile(angular.element(simpleHtml))(scope);
+        el.find('#edit').prevObject[0].click();
+
+        expect(scope.showEditForm).toHaveBeenCalled();
+    });
+
+    describe('scope.showEditForm() works', function() {
+        beforeEach(function() {
+            var record = {
+                time: "08:00 - 09:00",
+                title: "Andrey Vasilyev"
+            };
+            scope.showEditForm(record);
+        });
+
+        it('scope.showEditForm() update scope.editingRecord', function() {
+            expect(scope.editingRecord.time).toBeDefined();
+            expect(scope.editingRecord.title).toBeDefined();
+        });
+
+        it('scope.showEditForm() calls function createEditedEntityStorage, which creates scope.editingRecord', function() {
+            expect(scope.editedRecord.time).toBeDefined();
+            expect(scope.editedRecord.title).toBeDefined();
+        });
+    });
+
+    it('Clicks on "save" update record', function() {
+        spyOn(scope, 'updateRecord');
+        simpleHtml = '<span id="save" class="glyphicon glyphicon-ok" role="button" ng-click="updateRecord(record)"></span>';
+        el = $compile(angular.element(simpleHtml))(scope);
+        el.find('#save').prevObject[0].click();
+
+        expect(scope.updateRecord).toHaveBeenCalled();
+    });
+
+    describe('scope.updateRecord() works', function() {
+        var record = {
+            time: "08:00 - 09:00",
+            title: "Andrey Vasilyev"
+        };
+        beforeEach(function() {
+            scope.checkTitleValidation = function () { return true; };
+            scope.showInformModal = function () { return };
+        });
+
+        it('Title is not changed - modal shows', function() {
+            spyOn(scope, 'showInformModal');
+            scope.editedRecord = {
+                time: "08:00 - 09:00",
+                title: "Andrey Vasilyev"
+            };
+            scope.updateRecord(record);
+
+            expect(scope.showInformModal).toHaveBeenCalled();
+        });
+
+        describe('Record saves', function() {
+            beforeEach(function() {
+                spyOn(scope, 'showInformModal');
+                scope.editedRecord = {
+                    time: "08:00 - 09:00",
+                    title: "Ivan Vasilyev"
+                };
+                scope.updateRecord(record);
+            });
+
+            it('Title is changed - modal does not show', function() {
+                expect(scope.showInformModal).not.toHaveBeenCalled();
+            });
+
+            it('Record saved', function() {
+                expect(record.title).toEqual("Ivan Vasilyev");
+                expect(record.time).toEqual("08:00 - 09:00");
+            });
+
+            it('scope.editingRecord reseted', function() {
+                expect(scope.editingRecord ).toBeNull();
+            });
+        });
+    });
 });
--- src/js/Directives/addRecordDrct.js
+++ src/js/Directives/addRecordDrct.js
@@ -1,33 +1,33 @@
 app.directive("addRecord", function() {
-  return {
-    restrict: "AE",
-    link: function(scope) {
-      scope.newRecord = {};
-      resetNewRecord ();
-
-      scope.addRecord = function () {
-        var titleValid = scope.checkTitleValidation (scope.newRecord.title);
-        if (titleValid) {
-          applyNewTitle ();
-        }
-      };
-
-      function applyNewTitle () {
-        for (var i=0; i<scope.days[scope.chosenDate].length; i++) {
-          if (scope.days[scope.chosenDate][i].time === scope.newRecord.time) {
-            scope.days[scope.chosenDate][i].title = scope.newRecord.title;
-            scope.showInformModal('Record "' + scope.days[scope.chosenDate][i].title + '" successfully added!');
+    return {
+        restrict: "AE",
+        link: function(scope) {
+            scope.newRecord = {};
             resetNewRecord ();
-          }
-        }
-      }
 
-      function resetNewRecord () {
-        scope.newRecord.time = "";
-        scope.newRecord.title = "";
-      }
+            scope.addRecord = function () {
+                var titleValid = scope.checkTitleValidation (scope.newRecord.title);
+                if (titleValid) {
+                    applyNewTitle ();
+                }
+            };
+
+            function applyNewTitle () {
+                for (var i=0; i<scope.days[scope.chosenDate].length; i++) {
+                    if (scope.days[scope.chosenDate][i].time === scope.newRecord.time && scope.days[scope.chosenDate][i].title === "-") {
+                        scope.days[scope.chosenDate][i].title = scope.newRecord.title;
+                        scope.showInformModal('Record "' + scope.days[scope.chosenDate][i].title + '" successfully added!');
+                        resetNewRecord ();
+                    }
+                }
+            }
+
+            function resetNewRecord () {
+                scope.newRecord.time = "";
+                scope.newRecord.title = "";
+            }
+        }
     }
-  }
 });
 
 
